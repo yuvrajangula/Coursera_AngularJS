af28e54ab9c9bdb5bbf6f4a1447cb0abc4b83933
Yuvraj Angula <yuvrajangula@gmail.com>
2020-06-06 18:55:09 +0530

    implemented low cohesion and tight coupling

--- site/app.js
+++ site/app.js
@@ -3,19 +3,19 @@ var student = {
   type: "student"
 };
 
-document.addEventListener('DOMContentLoaded', contantLoaded);
+document.addEventListener('DOMContentLoaded', contentLoaded);
 
 function contentLoaded(event){
   document.getElementById('name').addEventListener("keyup", keyUp);
 }
 function keyUp(event) {
-  calculatorNumericOutput();
+  calculateNumericOutput();
 }
-function calculatorNumericOutput(){
+function calculateNumericOutput(){
   student.name = document.getElementById('name').value;
-  
+
 var totalNameValue = 0;
-for (var i = 0; i < array.length; i++) {
+for (var i = 0; i < student.name.length; i++) {
   totalNameValue += student.name.charCodeAt(i);
 }
 // Insert result into Page
